f15db132c6296f40ed13e2f70b581a8bf3ccdc91
Addie Addicted <addie@addicted.pw>
2018-01-13 10:19:34 -0500

    Cleaning up

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,6 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
   resources :places
-  get 'page/home'
-  get 'page/about'
-  get 'page/listings'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/about'  => 'page#about'
